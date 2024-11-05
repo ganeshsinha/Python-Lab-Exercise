5bbd34fa867334064105f329ea4cb95e36553250
Ganesh Sinha <ganesh.sinha@gmail.com>
2024-11-05 11:49:07 +0530

    Add files via upload

---
+++ Lab010.py
@@ -0,0 +1,38 @@
+
+#int
+x = 5
+print(type(x))
+
+#string
+x = "Hello world"
+print(type(x))
+
+#float
+x = 80.2
+print(type(x))
+
+#Complex
+x = 1j
+print(type(x))
+
+x = ["apple", "banana", "cherry"]
+
+#display x:
+print(x)
+print(x[0])
+print(x[1])
+print(x[2])
+
+#display the data type of x:
+print(type(x))
+
+x = ("apple", "banana", "cherry")
+
+#display x:
+print(x)
+print(x[0])
+print(x[1])
+print(x[2])
+
+#display the data type of x:
+print(type(x))
